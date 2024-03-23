d589bbd2005b2bafb0677ca45eef49bc7cab9895
Софья Илясова <116354773+kuzpugonka@users.noreply.github.com>
2024-03-23 12:55:58 +0300

    add ender pizza, edit card height

--- js/script.js
+++ js/script.js
@@ -23,21 +23,64 @@ const getPizzas = async () => {
   try {
     const response = await fetch(
       "https://cooperative-autumn-swordfish.glitch.me/api/products"
-      );
-      if (!response.ok) {
-        throw new Error('Ошибка в ответе сервера')
-      }
+    );
+    if (!response.ok) {
+      throw new Error("Ошибка в ответе сервера");
+    }
 
-      return await response.json()
-      
+    return await response.json();
   } catch (error) {
     console.error(`Данные с сервера не получены: ${error}`);
   }
 };
 
+const createCard = (data) => {
+  const card = document.createElement("article");
+  card.classList.add("card", "pizza__card");
+
+  card.innerHTML = `
+  <picture>
+  <source srcset="${data.images[1]}" type="imagt/webp">
+  <img
+  class="card__image"
+  src="${data.images[0]}"
+  alt="${data.name.ru}"
+  />
+  </picture>
+
+  <div class="card__content">
+    <h3 class="card__title">${data.name.ru}</h3>
+
+    <p class="card__info">
+      <span class="card__price">${data.price['25cm']} ₽</span>
+      <span>/</span>
+      <span class="card__weight">25 см</span>
+    </p>
+
+    <button class="card__button" data-id="${data.id}">Выбрать</button>
+  </div>
+
+  `;
+
+  return card;
+};
+
 const renderPizzas = async () => {
   const pizzas = await getPizzas();
-  console.log("pizzas: ", pizzas);
+  const pizzaList = document.querySelector(".pizza__list");
+  pizzaList.textContent = "";
+
+  const items = pizzas.map((data) => {
+    console.log('data: ', data);
+    const item = document.createElement("li");
+    item.classList.add("pizza__item");
+    const card = createCard(data);
+    item.append(card)
+
+    return item;
+  });
+
+  pizzaList.append(...items);
 };
 
 const init = () => {
