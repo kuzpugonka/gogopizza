d6ebedef938b9eb3b990ef71b5f55d35ddae6128
Софья Илясова <116354773+kuzpugonka@users.noreply.github.com>
2024-03-25 13:26:14 +0300

    add modules

---
+++ js/modules/toppingsToogle.js
@@ -0,0 +1,20 @@
+export const toppingsToogle = () => {
+  const toppingsButton = document.querySelector(".toppings__button");
+  const toppingsList = document.querySelector(".toppings__list");
+
+  toppingsButton.addEventListener("click", () => {
+    if (!toppingsList.classList.contains("toppings__list_show")) {
+      toppingsList.classList.add("toppings__list_show");
+      toppingsButton.classList.add("toppings__button_active");
+
+      toppingsList.style.maxHeight = toppingsList.scrollHeight + "px";
+    } else {
+      toppingsButton.classList.remove("toppings__button_active");
+      toppingsList.style.maxHeight = null;
+
+      setTimeout(() => {
+        toppingsList.classList.remove("toppings__list_show");
+      }, 300);
+    }
+  });
+};
