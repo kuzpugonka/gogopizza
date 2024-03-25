893bf16d21b416d9c5f6e445913075f9390ef3e8
Софья Илясова <116354773+kuzpugonka@users.noreply.github.com>
2024-03-25 13:21:48 +0300

    add loader

---
+++ js/modules/getData.js
@@ -0,0 +1,15 @@
+import { showLoader } from "./loader.js";
+
+export const getData = async (url) => {
+  showLoader();
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Ошибка в ответе сервера");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(`Данные с сервера не получены: ${error}`);
+  }
+};
