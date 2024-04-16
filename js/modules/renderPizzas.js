4634150f31a1efe7d1584031eacc4891d05aa4aa
Софья Илясова <116354773+kuzpugonka@users.noreply.github.com>
2024-04-16 12:32:03 +0300

    add renderno pizza

--- js/modules/renderPizzas.js
+++ js/modules/renderPizzas.js
@@ -1,5 +1,11 @@
 import { getData } from "./getData.js";
 
+const btnReset = document.querySelector("button");
+btnReset.classList.add("pizza__reset-toppings");
+btnReset.textContent = "Сбросить фильтр";
+btnReset.type = "reset";
+btnReset.setAttribute("form", "toppings");
+
 const createCard = (data) => {
   const card = document.createElement("article");
   card.classList.add("card", "pizza__card");
@@ -39,18 +45,29 @@ export const renderPizzas = async (toppings) => {
       toppings ? `?toppings=${toppings}` : ""
     }`
   );
+  const pizzaTitle = document.querySelector(".pizza__title");
   const pizzaList = document.querySelector(".pizza__list");
   pizzaList.textContent = "";
 
-  const items = pizzas.map((data) => {
-    // console.log("data: ", data);
-    const item = document.createElement("li");
-    item.classList.add("pizza__item");
-    const card = createCard(data);
-    item.append(card);
+  if (pizzas.length) {
+    pizzaTitle.textContent = "Пицца";
 
-    return item;
-  });
+    const items = pizzas.map((data) => {
+      const item = document.createElement("li");
+      item.classList.add("pizza__item");
+      const card = createCard(data);
+      item.append(card);
+      return item;
+    });
 
-  pizzaList.append(...items);
+    pizzaList.append(...items);
+  } else {
+    pizzaTitle.textContent = "Такой пиццы у нас нет :(";
+    pizzaTitle.after(btnReset);
+  }
 };
+
+btnReset.addEventListener("click", () => {
+  renderPizzas();
+  document.querySelector("toppings__reset").remove();
+});
